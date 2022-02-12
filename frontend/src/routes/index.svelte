0ec168239c8698d720b9054316bed896580b779a
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2022-02-12 12:59:35 +0200

    Svelte UI added

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from "svelte";
+
+    let dbs = [];
+
+    onMount(async function() {
+        const response = await fetch('http://localhost:8080/dbs');
+        dbs = await response.json();
+    });
+</script>
+<h1>Welcome to GoGlacier</h1>
+<p>Choose a backup DB:</p>
+
+<ul>
+    {#each dbs as db}
+    <li>{db}</li>
+    {/each}
+</ul>
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func databases(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(http.StatusOK)
+	dbs := []string{}
+	dbs = append(dbs, "Hello")
+	dbs = append(dbs, "World")
+	json.NewEncoder(w).Encode(dbs)
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	http.Handle("/dbs", http.HandlerFunc(databases))
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
